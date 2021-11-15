9b24b331b617e2c310265589de3784a97638f6bc
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-15 21:13:46 +0300

    Route for updating a blog according to blog id

--- app/main/views.py
+++ app/main/views.py
@@ -89,3 +89,20 @@ def blog(id):
     blog = Blog.query.get(id)
     return render_template('blog.html',blog=blog,comments=comments)
    
+@main.route('/blog/<blog_id>/update', methods = ['GET','POST'])
+@login_required
+def updateblog(blog_id):
+    blog = Blog.query.get(blog_id)
+    if blog.user != current_user:
+        abort(403)
+    form = CreateBlog()
+    if form.validate_on_submit():
+        blog.title = form.title.data
+        blog.content = form.content.data
+        db.session.commit()
+        flash("Awesome...Your blog has been updated!")
+        return redirect(url_for('main.blog',id = blog.id)) 
+    if request.method == 'GET':
+        form.title.data = blog.title
+        form.content.data = blog.content
+    return render_template('newblog.html', form = form)
