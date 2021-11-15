e11ee0af1a7de8a62a86dba113cca610cd28f7be
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-15 20:34:10 +0300

    Creating the update profile form

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,3 +4,10 @@ from wtforms import StringField,TextAreaField, SubmitField,ValidationError
 from wtforms.validators import Required,Email
 from flask_login import current_user
 from ..models import User
+
+class UpdateProfile(FlaskForm):
+    username = StringField('Your username here..', validators=[Required()])
+    email = StringField('Your email address here..', validators=[Required(),Email()])
+    bio = TextAreaField('Tell us about you here..',validators = [Required()])
+    profile_picture = FileField('profile picture', validators=[FileAllowed(['jpg','png'])])
+    submit = SubmitField('Update')
