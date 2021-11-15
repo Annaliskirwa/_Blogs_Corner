9e313c3b7fdb41f62e579999cb7a59b8c53529e6
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-15 20:10:46 +0300

    Creating the user model

--- app/models.py
+++ app/models.py
@@ -6,3 +6,36 @@ from datetime import datetime
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
+
+class User (UserMixin,db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),unique = True,nullable = False)
+    email = db.Column(db.String(255), unique = True,nullable = False)
+    bio = db.Column(db.String(255),default ='Bio will be here shortly..')
+    profile_pic_path = db.Column(db.String(150),default ='default.png')
+    hashed_password = db.Column(db.String(255),nullable = False)
+    blog = db.relationship('Blog', backref='user', lazy='dynamic')
+    comment = db.relationship('Comment', backref='user', lazy='dynamic')
+    
+    @property
+    def set_password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @set_password.setter
+    def password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.hashed_password,password)
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return "User: %s" %str(self.username)
