46679f4976df4d3a95a1531f9e3e3ee228279fd3
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-15 21:09:09 +0300

    Updating the user profile route

--- app/main/views.py
+++ app/main/views.py
@@ -42,7 +42,7 @@ def profile():
         current_user.email = form.email.data
         current_user.bio = form.bio.data
         db.session.commit()
-        flash('Succesfully updated your profile')
+        flash('Your profile has been updated..')
         return redirect(url_for('main.profile'))
     elif request.method == 'GET':
         form.username.data = current_user.username
@@ -50,3 +50,18 @@ def profile():
         form.bio.data = current_user.bio
     profile_pic_path = url_for('static',filename = 'photos/'+ current_user.profile_pic_path) 
     return render_template('profile/profile.html', profile_pic_path=profile_pic_path, form = form,blog=blog)
+
+@main.route('/user/<name>/updateprofile', methods = ['POST','GET'])
+@login_required
+def updateprofile(name):
+    form = UpdateProfile()
+    user = User.query.filter_by(username = name).first()
+    if user == None:
+        abort(404)
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+        user.save()
+        return redirect(url_for('.profile',name = name))
+    return render_template('profile/updateprofile.html',form =form)
+
+
