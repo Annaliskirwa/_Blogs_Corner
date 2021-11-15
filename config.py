9d64dfe5147b9e0f5a6e757892b5f019d0ca52ff
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-15 19:48:41 +0300

    Configuration classes, email and database configs

---
+++ config.py
@@ -0,0 +1,36 @@
+import os 
+
+class Config:
+    SECRET_KEY = 'annisawesome'
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://annalis:Ann123@localhost/project"
+    UPLOADED_PHOTOS_DEST = "app/static/photos"
+
+    # email configurations
+    MAIL_SERVER = "smtp.gmail.com"
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+class ProdConfig(Config):
+    
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+
+class TestConfig(Config):
+    
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://annalis:Ann123@localhost/project"
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://annalis:Ann123@localhost/project"
+    DEBUG = True
+
+
+config_options = {
+    "development": DevConfig,
+    "production": ProdConfig,
+    "test": TestConfig
+}
+
+
