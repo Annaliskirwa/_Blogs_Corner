ff8d6f06ea9d13798156037add4103493ddcb7df
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-15 23:06:49 +0300

    The registration form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -9,3 +9,15 @@ class LoginForm(FlaskForm):
     password = PasswordField('Enter here your password..',validators=[Required()])
     remember = BooleanField('Remember Me!')
     submit = SubmitField('Login')
+
+class RegForm(FlaskForm):
+    username = StringField('Enter here your username..', validators=[Required(), Length(min=4, max=20)])
+    email = StringField('Enter here your email address..', validators=[Required(),Email()])
+    password = PasswordField('Enter here your email password..',validators = [Required(), EqualTo('password_confirm',message = 'Your passwords should match')])
+    password_confirm = PasswordField('Confirm your password above..',validators = [Required()])
+    submit = SubmitField('Sign Up')
+
+    def validate_email(self,data_field):
+        if User.query.filter_by(email = data_field.data).first():
+            raise ValidationError(message="Ooops!That email has already been taken..")
+    
